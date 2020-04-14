58115cb5896d87dbcd34569050e865c13a39e260
petergartland <petergartland@ucsb.edu>
2020-04-13 23:05:26 -0400

    conflict

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 #include<iostream>
 #include"functions.h"
 int main()
-{
+{//nothing
 	print_hello();
 	std::cout<<std::endl;
-	std::cout<<"test"<<std::endl;
+	std::cout<<"testtt"<<std::endl;
 	int num;
 	std::cin>>num;
 	std::cout<< "The factorial of " << num << " is: " << factorial(num)<<std::endl;
