16a5b20c0e2c3434e4b0ea0b5eab4bf41f7cac05
petergartland <petergartland@ucsb.edu>
2020-04-13 23:17:19 -0400

    new branch

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ int main()
 	std::cout<<"testtt"<<std::endl;
 	std::cout<<"testt"<<std::endl;
 	int num;
+	std::cout<<"stuff"<<std::endl;
 	std::cin>>num;
 	std::cout<< "The factorial of " << num << " is: " << factorial(num)<<std::endl;
 	return 0;
